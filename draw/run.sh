d6cc602a79e31fc7bea580fa1701ee7decb53559
Lokiraze <lokiraze@noshamegames.com>
2024-09-21 16:00:44 -0700

    maybe?

--- draw/run.sh
+++ draw/run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 docker build -t keymap ./draw
-docker run --rm --name keymap -v $(pwd):/app keymap keymap -c "./draw/config.yaml" parse -z "./config/corne.keymap"
+docker run --rm --name keymap -v $(pwd):/app keymap keymap -v
 # docker run --rm --name keymap -v $(pwd):/app keymap keymap -c "draw/config.yaml" parse -z "config/corne.keymap"
 # keymap -c "./config.yaml" parse -z "{{ config }}/base.keymap" >"{{ draw }}/base.yaml"
 # keymap -c "./config.yaml" draw "{{ draw }}/base.yaml" -k "ferris/sweep" >"{{ draw }}/base.svg"
